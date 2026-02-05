522679dcfd11c7687aff84f9ca214f249d1bbfcf
Jeff Ahn <ahn.jeff@gmail.com>
2026-02-05 13:35:20 -0800

    change flag name to --example in compileshaders.py

--- shaders/glsl/compileshaders.py
+++ shaders/glsl/compileshaders.py
@@ -9,9 +9,10 @@ import sys
 
 parser = argparse.ArgumentParser(description='Compile all GLSL shaders')
 parser.add_argument('--glslang', type=str, help='path to glslangvalidator executable')
-parser.add_argument('--sample', type=str, help='can be used to compile shaders for a single sample only')
+parser.add_argument('--example', type=str, help='can be used to compile shaders for a single example only')
 parser.add_argument('--g', action='store_true', help='compile with debug symbols')
 args = parser.parse_args()
+
 
 def findGlslang():
     def isExe(path):
@@ -31,11 +32,13 @@ def findGlslang():
 
     sys.exit("Could not find glslangvalidator executable on PATH, and was not specified with --glslang")
 
-file_extensions = tuple([".vert", ".frag", ".comp", ".geom", ".tesc", ".tese", ".rgen", ".rchit", ".rmiss", ".mesh", ".task"])
+
+file_extensions = tuple(
+    [".vert", ".frag", ".comp", ".geom", ".tesc", ".tese", ".rgen", ".rchit", ".rmiss", ".mesh", ".task"])
 
 compile_single_sample = ""
-if args.sample != None:
-    compile_single_sample = args.sample
+if args.example != None:
+    compile_single_sample = args.example
     if (not os.path.isdir(compile_single_sample)):
         print("ERROR: No directory found with name %s" % compile_single_sample)
         exit(-1)
@@ -59,7 +62,7 @@ for root, dirs, files in os.walk(dir_path):
 
             # Ray tracing shaders require a different target environment           
             if file.endswith(".rgen") or file.endswith(".rchit") or file.endswith(".rmiss"):
-               add_params = add_params + " --target-env vulkan1.2"
+                add_params = add_params + " --target-env vulkan1.2"
             # Same goes for samples that use ray queries
             if root.endswith("rayquery") and file.endswith(".frag"):
                 add_params = add_params + " --target-env vulkan1.2"
